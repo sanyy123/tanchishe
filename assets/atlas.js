1fc21d35dbdf6c37efd185aa09947c2f56e5721d
sanyy123 <tms9s6ht5h@privaterelay.appleid.com>
2026-09-13 14:45:37 +0800

    Add files via upload

---
+++ assets/atlas.js
@@ -0,0 +1,639 @@
+window.ATLAS_DATA = {
+  "frames": {
+    "food.png": {
+      "frame": {
+        "x": 2,
+        "y": 2,
+        "w": 794,
+        "h": 462
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 75,
+        "y": 302,
+        "w": 794,
+        "h": 462
+      },
+      "sourceSize": {
+        "w": 1024,
+        "h": 1024
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "leaf.png": {
+      "frame": {
+        "x": 800,
+        "y": 2,
+        "w": 762,
+        "h": 718
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 144,
+        "y": 145,
+        "w": 762,
+        "h": 718
+      },
+      "sourceSize": {
+        "w": 1024,
+        "h": 1024
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "cat_head.png": {
+      "frame": {
+        "x": 1566,
+        "y": 2,
+        "w": 389,
+        "h": 261
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 65,
+        "y": 4,
+        "w": 389,
+        "h": 261
+      },
+      "sourceSize": {
+        "w": 527,
+        "h": 473
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "head.png": {
+      "frame": {
+        "x": 1566,
+        "y": 267,
+        "w": 354,
+        "h": 280
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 69,
+        "y": 95,
+        "w": 354,
+        "h": 280
+      },
+      "sourceSize": {
+        "w": 500,
+        "h": 500
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "sheep_tail.png": {
+      "frame": {
+        "x": 1924,
+        "y": 267,
+        "w": 116,
+        "h": 141
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 18,
+        "y": 4,
+        "w": 116,
+        "h": 141
+      },
+      "sourceSize": {
+        "w": 148,
+        "h": 150
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "sheep_head.png": {
+      "frame": {
+        "x": 1566,
+        "y": 551,
+        "w": 330,
+        "h": 263
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 26,
+        "y": 63,
+        "w": 330,
+        "h": 263
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "snake_drop.png": {
+      "frame": {
+        "x": 1900,
+        "y": 551,
+        "w": 146,
+        "h": 197
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 115,
+        "y": 116,
+        "w": 146,
+        "h": 197
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "snake_tail.png": {
+      "frame": {
+        "x": 1900,
+        "y": 752,
+        "w": 139,
+        "h": 211
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 17,
+        "y": 21,
+        "w": 139,
+        "h": 211
+      },
+      "sourceSize": {
+        "w": 177,
+        "h": 264
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "sheep_decor.png": {
+      "frame": {
+        "x": 2,
+        "y": 468,
+        "w": 280,
+        "h": 274
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 51,
+        "y": 53,
+        "w": 280,
+        "h": 274
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "snake_head.png": {
+      "frame": {
+        "x": 286,
+        "y": 468,
+        "w": 276,
+        "h": 205
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 54,
+        "y": 85,
+        "w": 276,
+        "h": 205
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "horse_tail.png": {
+      "frame": {
+        "x": 566,
+        "y": 468,
+        "w": 223,
+        "h": 332
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 81,
+        "y": 25,
+        "w": 223,
+        "h": 332
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "horse_head.png": {
+      "frame": {
+        "x": 286,
+        "y": 724,
+        "w": 275,
+        "h": 236
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 49,
+        "y": 45,
+        "w": 275,
+        "h": 236
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "sheep_food.png": {
+      "frame": {
+        "x": 2,
+        "y": 746,
+        "w": 274,
+        "h": 286
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 55,
+        "y": 50,
+        "w": 274,
+        "h": 286
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "snake_food.png": {
+      "frame": {
+        "x": 793,
+        "y": 724,
+        "w": 261,
+        "h": 259
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 58,
+        "y": 67,
+        "w": 261,
+        "h": 259
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "rab_head.png": {
+      "frame": {
+        "x": 565,
+        "y": 818,
+        "w": 206,
+        "h": 234
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 51,
+        "y": 30,
+        "w": 206,
+        "h": 234
+      },
+      "sourceSize": {
+        "w": 308,
+        "h": 308
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "rab_paw.png": {
+      "frame": {
+        "x": 565,
+        "y": 1056,
+        "w": 204,
+        "h": 178
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 50,
+        "y": 65,
+        "w": 204,
+        "h": 178
+      },
+      "sourceSize": {
+        "w": 308,
+        "h": 308
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "dragon_head.png": {
+      "frame": {
+        "x": 280,
+        "y": 964,
+        "w": 257,
+        "h": 205
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 24,
+        "y": 38,
+        "w": 257,
+        "h": 205
+      },
+      "sourceSize": {
+        "w": 308,
+        "h": 308
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "rab_body.png": {
+      "frame": {
+        "x": 2,
+        "y": 1036,
+        "w": 237,
+        "h": 218
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 33,
+        "y": 45,
+        "w": 237,
+        "h": 218
+      },
+      "sourceSize": {
+        "w": 308,
+        "h": 308
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "dragon_decor.png": {
+      "frame": {
+        "x": 243,
+        "y": 1173,
+        "w": 231,
+        "h": 151
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 39,
+        "y": 77,
+        "w": 231,
+        "h": 151
+      },
+      "sourceSize": {
+        "w": 308,
+        "h": 308
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "body.png": {
+      "frame": {
+        "x": 2,
+        "y": 1258,
+        "w": 202,
+        "h": 199
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 53,
+        "y": 55,
+        "w": 202,
+        "h": 199
+      },
+      "sourceSize": {
+        "w": 308,
+        "h": 308
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "dragon_tail.png": {
+      "frame": {
+        "x": 2,
+        "y": 1461,
+        "w": 158,
+        "h": 290
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 112,
+        "y": 46,
+        "w": 158,
+        "h": 290
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "horse_decor.png": {
+      "frame": {
+        "x": 2,
+        "y": 1755,
+        "w": 201,
+        "h": 234
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 88,
+        "y": 79,
+        "w": 201,
+        "h": 234
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "rab_food.png": {
+      "frame": {
+        "x": 164,
+        "y": 1461,
+        "w": 150,
+        "h": 189
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 81,
+        "y": 59,
+        "w": 150,
+        "h": 189
+      },
+      "sourceSize": {
+        "w": 308,
+        "h": 308
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "snake_leaf.png": {
+      "frame": {
+        "x": 207,
+        "y": 1654,
+        "w": 149,
+        "h": 174
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 116,
+        "y": 110,
+        "w": 149,
+        "h": 174
+      },
+      "sourceSize": {
+        "w": 384,
+        "h": 384
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "horse_food.png": {
+      "frame": {
+        "x": 318,
+        "y": 1832,
+        "w": 157,
+        "h": 170
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 28,
+        "y": 36,
+        "w": 157,
+        "h": 170
+      },
+      "sourceSize": {
+        "w": 215,
+        "h": 230
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    },
+    "dragon_food.png": {
+      "frame": {
+        "x": 479,
+        "y": 1832,
+        "w": 159,
+        "h": 166
+      },
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {
+        "x": 73,
+        "y": 71,
+        "w": 159,
+        "h": 166
+      },
+      "sourceSize": {
+        "w": 308,
+        "h": 308
+      },
+      "pivot": {
+        "x": 0.5,
+        "y": 0.5
+      }
+    }
+  },
+  "meta": {
+    "app": "https://free-tex-packer.org",
+    "version": "0.6.7",
+    "image": "atlas.png",
+    "format": "RGBA8888",
+    "size": {
+      "w": 2048,
+      "h": 2048
+    },
+    "scale": 1
+  }
+};
